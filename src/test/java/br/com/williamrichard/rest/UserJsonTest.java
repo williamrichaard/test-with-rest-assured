c80f424c7d25c167ebfe4d67b615913fc476e837
williamrichaard <wrichaard@hotmail.com>
2022-04-26 22:55:38 -0300

    Added test verify root list :test_tube:

--- src/test/java/br/com/williamrichard/rest/UserJsonTest.java
+++ src/test/java/br/com/williamrichard/rest/UserJsonTest.java
@@ -94,6 +94,7 @@ public class UserJsonTest {
             .body("$", hasSize(3)) //pode deixar "" branco retirando o $
             .body("name", hasItems("João da Silva", "Maria Joaquina", "Ana Júlia"))
             .body("age[1]", is(25))
-            .body("filhos.name", hasItem(Arrays.asList("Zezinho", "Luizinho")));
+            .body("filhos.name", hasItem(Arrays.asList("Zezinho", "Luizinho")))
+            .body("salary", contains(1234.5678f, 2500, null));
     }
 }

91a65ebdee47e4fd546f3107e23d4910a3241078
williamrichaard <wrichaard@hotmail.com>
2022-04-27 22:40:35 -0300

    Added test advanced checks :test_tube:

--- src/test/java/br/com/williamrichard/rest/UserJsonTest.java
+++ src/test/java/br/com/williamrichard/rest/UserJsonTest.java
@@ -107,7 +107,7 @@ public class UserJsonTest {
             .statusCode(200)
             .body("$", hasSize(3))
             .body("age.findAll{it <= 25}.size()", is(2))
-            .body("age.findAll{it <= 25 && it > 20}.size()", is(1));
-
+            .body("age.findAll{it <= 25 && it > 20}.size()", is(1))
+            .body("findAll{it.age <= 25 && it.age > 20}.name", hasItem("Maria Joaquina"));
     }
 }

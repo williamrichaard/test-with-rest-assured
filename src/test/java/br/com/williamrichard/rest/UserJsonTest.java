3c37413ce7221f7ec384fa757aaaae7a21472734
williamrichaard <wrichaard@hotmail.com>
2022-04-30 23:00:12 -0300

    Added test advanced checks :test_tube:

--- src/test/java/br/com/williamrichard/rest/UserJsonTest.java
+++ src/test/java/br/com/williamrichard/rest/UserJsonTest.java
@@ -111,6 +111,7 @@ public class UserJsonTest {
             .body("findAll{it.age <= 25 && it.age > 20}.name", hasItem("Maria Joaquina"))
             .body("findAll{it.age <= 25}[0].name", is("Maria Joaquina"))
             .body("findAll{it.age <= 25}[-1].name", is("Ana Júlia"))
-            .body("find{it.age <= 25}.name", is("Maria Joaquina"));
+            .body("find{it.age <= 25}.name", is("Maria Joaquina"))
+            .body("findAll{it.name.contains('n')}.name", hasItems("Maria Joaquina", "Ana Júlia"));
     }
 }

763c2a5bd6f8c21c10d878fe4602ba26ebe07fd8
williamrichaard <wrichaard@hotmail.com>
2022-05-04 01:45:02 -0300

    Added test using XML :test_tube:

--- src/test/java/br/com/williamrichard/rest/UserXMLTest.java
+++ src/test/java/br/com/williamrichard/rest/UserXMLTest.java
@@ -2,10 +2,14 @@ package br.com.williamrichard.rest;
 
 import org.junit.Test;
 
+import static io.restassured.RestAssured.given;
+
 public class UserXMLTest {
 
     @Test
     public void devoTrabalharComXML() {
-
+        given()
+        .when()
+        .then();
     }
 }

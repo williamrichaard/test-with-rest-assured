6487ef0bd68ecdbfdc36a7f953a71d8a0643cf68
williamrichaard <wrichaard@hotmail.com>
2022-05-16 22:27:24 -0300

    Added test using XML :test_tube:

--- src/test/java/br/com/williamrichard/rest/UserXMLTest.java
+++ src/test/java/br/com/williamrichard/rest/UserXMLTest.java
@@ -43,6 +43,6 @@ public class UserXMLTest {
             .body("users.user.find{it.age == 25}.name", is("Maria Joaquina"))
             .body("users.user.findAll{it.name.toString().contains('n')}.name", hasItems("Maria Joaquina", "Ana Julia"))
             .body("users.user.salary.find{it != null}", is("1234.5678"))
-        ;
+        ;;;
     }
 }
